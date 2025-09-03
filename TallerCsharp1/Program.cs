c660d4cabaacc19b9b768c152d03efcf12894f07
Fiorella <Fiorella@LAPTOP-QG0TP808>
2025-09-03 01:43:34 -0500

    primer cambio
    Añadir funcionalidad de cálculo de producto

--- TallerCsharp1/Program.cs
+++ TallerCsharp1/Program.cs
@@ -10,6 +10,32 @@ namespace TallerCsharp1
     {
         static void Main(string[] args)
         {
+            //Escribir un titulo o descripcion
+            Console.WriteLine("Bueno, este es el tercer intento jsjsjjs");
+
+            //Definir variables
+            int a, b, producto;
+
+            //Entrada de datos
+            Console.WriteLine("Ingrese el primer numero, porfis: ");
+            //Guardar valores en la consola
+            a = int.Parse(Console.ReadLine());
+            //a = Convert.ToInt32(Console.ReadLine());
+            //a = int.TryParse(Console.ReadLine());me devuelve un booleano true o false
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            //Proceso
+            producto = a * b;
+
+            //Salida de datos
+            Console.WriteLine("El producto es: " + (producto+2));
+            Console.WriteLine($"El producto de {a}*{b}={producto + 2}");
+
+            //Detener la pantalla
+            Console.ReadKey();
+
         }
     }
 }
